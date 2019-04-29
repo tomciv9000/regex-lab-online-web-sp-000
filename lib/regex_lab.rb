9cb6603cb55058cfcbad4a92702edc7ef8c9ed54
Tom White <eugene.white@gmail.com>
2019-04-29 04:41:53 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
 def starts_with_a_vowel?(word)
-  !!word.match(/^[aeiouAEIOU]+\w/)
+  !!word.match(/^[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un+\w+ing/)
+ text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\b\w[a-z]{4}\b/)
+ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  !!text.match(/^[A-Z].+\W$/)
+  !!text.match(/^[A-Z].+[\.\?!]$/)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\d/).count == 10
+  if phone.match(/\D?[0-9]{3}\D?[0-9]{3}\D?[0-9]{4}/)
+    return true 
+  else
+    return false 
+  end
 end
